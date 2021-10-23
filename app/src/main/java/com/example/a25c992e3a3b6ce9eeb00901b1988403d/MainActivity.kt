13fb46e009faeb9d897c9aeaece83367845c7d8f
UMUTC <umut.cansu@intertech.com.tr>
2021-10-23 12:35:14 +0300

    init project

---
+++ app/src/main/java/com/example/a25c992e3a3b6ce9eeb00901b1988403d/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a25c992e3a3b6ce9eeb00901b1988403d
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
